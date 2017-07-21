d6410a4c17178751702425a1cf215f603ee5c8c7
_Thanhbv_ <30338652+thanhbv21@users.noreply.github.com>
2017-07-21 16:01:43 +0700

    Add project files.

---
+++ Scannercheck/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+using Android.Views;
+using Android.Graphics;
+
+using ZXing;
+using ZXing.Mobile;
+
+namespace Scannercheck
+{
+    [Activity(Label = "Scanner check", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        ImageButton buttonScan;
+
+        MobileBarcodeScanner scanner;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            MobileBarcodeScanner.Initialize(Application);
+            SetContentView (Resource.Layout.Main);
+
+            scanner = new MobileBarcodeScanner();
+            buttonScan = this.FindViewById<ImageButton>(Resource.Id.scanbutton);
+            buttonScan.Click += async delegate
+            {
+                scanner.UseCustomOverlay = false;
+
+                scanner.TopText = "Giữ camera cách mã khoảng 6inch";
+                scanner.BottomText = "Đợi một chút...";
+
+                //Start scanning
+                var result = await scanner.Scan();
+
+                HandleScanResult(result);
+            };
+
+        }
+
+        void HandleScanResult(ZXing.Result result)
+        {
+            string msg = "";
+
+            if (result != null && !string.IsNullOrEmpty(result.Text))
+                msg = "Found Barcode: " + result.Text;
+            else
+                msg = "Scanning Canceled!";
+
+            this.RunOnUiThread(() => Toast.MakeText(this, msg, ToastLength.Short).Show());
+        }
+    }
+}
+
